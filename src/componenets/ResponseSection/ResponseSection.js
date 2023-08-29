526115268fcc2be2d4dec95a26a84d13495675eb
rajat <rajatsingharora27@gmail.com>
2023-08-29 14:51:31 +0530

    seperated components

--- src/componenets/ResponseSection/ResponseSection.js
+++ src/componenets/ResponseSection/ResponseSection.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const ResponseSection = () => {
   return (
-    <div className="w-1/2 text-black border-solid border-2 border-black h-[100vh] rounded-md p-8">
-      ResponseSection
-    </div>
+    <div className="w-1/2 text-black border-solid border-2 border-black h-[100vh] rounded-md p-8"></div>
   );
 };
 
